691966a0dcd552ffbd8aa11bd591fa2ec2be424e
suesspas <pascal.suess@googlemail.com>
2022-11-30 17:04:26 +0100

    Skittle Textures

--- desktop/src/de/cerus/cookieclicker/desktop/DesktopLauncher.java
+++ desktop/src/de/cerus/cookieclicker/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package de.cerus.cookieclicker.desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import de.cerus.cookieclicker.CookieClickerGame;
@@ -9,9 +10,11 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.backgroundFPS = 30;
-		config.title = "Cookie Clicker";
+		config.title = "Skittle Clicker";
 		config.width = 854;
 		config.height = 480;
+		config.addIcon("skittle_icon32.png", Files.FileType.Internal);
+		config.addIcon("skittle_icon16.png", Files.FileType.Internal);
 
 		new LwjglApplication(new CookieClickerGame(), config);
 	}
