1ad987c3d68548b458217207297577b311a823bf
suesspas <pascal.suess@googlemail.com>
2022-11-30 20:25:14 +0100

    replaced old window application with newer version

--- desktop/src/de/cerus/cookieclicker/desktop/DesktopLauncher.java
+++ desktop/src/de/cerus/cookieclicker/desktop/DesktopLauncher.java
@@ -2,20 +2,19 @@ package de.cerus.cookieclicker.desktop;
 
 import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import de.cerus.cookieclicker.CookieClickerGame;
 
 public class DesktopLauncher {
 
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.backgroundFPS = 30;
-		config.title = "Skittle Clicker";
-		config.width = 854;
-		config.height = 480;
-		config.addIcon("skittle_icon32.png", Files.FileType.Internal);
-		config.addIcon("skittle_icon16.png", Files.FileType.Internal);
-
-		new LwjglApplication(new CookieClickerGame(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setIdleFPS(30);
+		config.setTitle("Skittle Clicker");
+		config.setWindowedMode(800, 480);
+		config.useVsync(true);
+		config.setWindowIcon("skittle_icon32.png");
+		new Lwjgl3Application(new CookieClickerGame(), config);
 	}
 }
